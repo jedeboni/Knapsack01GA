3627f8cb2328bc84345889494b32cf3808b9b1b8
Jose Eduardo Deboni <jose@eduardodeboni.com>
2016-04-09 14:06:30 -0300

    Versão inicial reorganizada

---
+++ KnapsackGA/KnapsackGA/Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnapsackGA
+{
+    public class Item
+    {
+        public String descricao; //  descrição do item
+        public double peso; // peso do item
+        public double valor; // valor do item
+
+
+        public Item(String nome,  double p, double v)
+        {
+           descricao = nome;
+            valor = v;
+            peso = p;
+        }
+    }
+}
